6db2427490fd7524e73cb7ebfd2179e4f2707c79
xharlie <charlie.xu007@yahoo.com>
2020-02-11 20:27:40 -0800

    new c

--- preprocessing/create_ivt.py
+++ preprocessing/create_ivt.py
@@ -20,73 +20,29 @@ parser.add_argument('--category', type=str, default="all", help='Which single cl
                                                                 'be chair or plane, etc.]')
 FLAGS = parser.parse_args()
 
-def get_sdf_value(sdf_pt, sdf_params_ph, sdf_ph, sdf_res):
-    x = np.linspace(sdf_params_ph[0], sdf_params_ph[3], num=sdf_res+1)
-    y = np.linspace(sdf_params_ph[1], sdf_params_ph[4], num=sdf_res+1)
-    z = np.linspace(sdf_params_ph[2], sdf_params_ph[5], num=sdf_res+1)
-    my_interpolating_function = RegularGridInterpolator((z, y, x), sdf_ph)
-    sdf_value = my_interpolating_function(sdf_pt)
-    print("sdf_value:", sdf_value.shape)
-    return np.expand_dims(sdf_value, axis=1)
+def get_unigrid(ivt_res, uni_num):
+    grids = 1 / ivt_res 
+    x = np.linspace(-1, 1, num=grids).astype(np.float32)
+    y = np.linspace(-1, 1, num=grids).astype(np.float32)
+    z = np.linspace(-1, 1, num=grids).astype(np.float32)
+    choicex = np.random.randint(len(ind), size=uni_num)
+    choicey = np.random.randint(len(ind), size=uni_num)
+    choicez = np.random.randint(len(ind), size=uni_num)
+    x_vals = x[choicex]
+    y_vals = x[choicey]
+    z_vals = x[choicez]
+    s = np.random.normal(mu, sigma, 1000)
 
-def get_sdf(sdf_file, sdf_res):
-    intsize = 4
-    floatsize = 8
-    sdf = {
-        "param": [],
-        "value": []
-    }
-    with open(sdf_file, "rb") as f:
-        try:
-            bytes = f.read()
-            ress = np.fromstring(bytes[:intsize * 3], dtype=np.int32)
-            if -1 * ress[0] != sdf_res or ress[1] != sdf_res or ress[2] != sdf_res:
-                raise Exception(sdf_file, "res not consistent with ", str(sdf_res))
-            positions = np.fromstring(bytes[intsize * 3:intsize * 3 + floatsize * 6], dtype=np.float64)
-            # bottom left corner, x,y,z and top right corner, x, y, z
-            sdf["param"] = [positions[0], positions[1], positions[2],
-                            positions[3], positions[4], positions[5]]
-            sdf["param"] = np.float32(sdf["param"])
-            sdf["value"] = np.fromstring(bytes[intsize * 3 + floatsize * 6:], dtype=np.float32)
-            sdf["value"] = np.reshape(sdf["value"], (sdf_res + 1, sdf_res + 1, sdf_res + 1))
-        finally:
-            f.close()
-    return sdf
 
-def get_offset_ball(num, bandwidth):
-    u = np.random.normal(0, 1, size=(num,1))
-    v = np.random.normal(0, 1, size=(num,1))
-    w = np.random.normal(0, 1, size=(num,1))
-    r = np.random.uniform(0, 1, size=(num,1)) ** (1. / 3) * bandwidth
-    norm = np.linalg.norm(np.concatenate([u, v, w], axis=1),axis=1, keepdims=1)
-    # print("u.shape",u.shape)
-    # print("norm.shape",norm.shape)
-    # print("r.shape",r.shape)
-    (x, y, z) = r * (u, v, w) / norm
-    return np.concatenate([x,y,z],axis=1)
-
-def get_offset_cube(num, bandwidth):
-    u = np.random.normal(0, 1, size=(num,1))
-    v = np.random.normal(0, 1, size=(num,1))
-    w = np.random.normal(0, 1, size=(num,1))
-    r = np.random.uniform(0, 1, size=(num,1)) ** (1. / 3) * bandwidth
-    norm = np.linalg.norm(np.concatenate([u, v, w], axis=1),axis=1, keepdims=1)
-    (x, y, z) = r * (u, v, w) / norm
-    return np.concatenate([x,y,z],axis=1)
-
-def sample_sdf(cat_id, num_sample, bandwidth, iso_val, sdf_dict, sdf_res):
+def sample_ivt_uni(cat_id, num_sample, ivt_res):
     start = time.time()
-    percentages = [[-1. * bandwidth, -1. * bandwidth * 0.30, int(num_sample * 0.25)],
-                  [-1. * bandwidth * 0.30, 0, int(num_sample * 0.25)],
-                  [0, bandwidth * 0.30, int(num_sample * 0.25)],
-                  [bandwidth * 0.30, bandwidth, int(num_sample * 0.25)]]
     params = sdf_dict["param"]
     sdf_values = sdf_dict["value"].flatten()
     # print("np.min(sdf_values), np.mean(sdf_values), np.max(sdf_values)",
     #       np.min(sdf_values), np.mean(sdf_values), np.max(sdf_values))
-    x = np.linspace(params[0], params[3], num=sdf_res + 1).astype(np.float32)
-    y = np.linspace(params[1], params[4], num=sdf_res + 1).astype(np.float32)
-    z = np.linspace(params[2], params[5], num=sdf_res + 1).astype(np.float32)
+    x = np.linspace(params[0], params[3], num=ivt_res + 1).astype(np.float32)
+    y = np.linspace(params[1], params[4], num=ivt_res + 1).astype(np.float32)
+    z = np.linspace(params[2], params[5], num=ivt_res + 1).astype(np.float32)
     dis = sdf_values - iso_val
     sdf_pt_val = np.zeros((0,4), dtype=np.float32)
     for i in range(len(percentages)):
@@ -100,9 +56,9 @@ def sample_sdf(cat_id, num_sample, bandwidth, iso_val, sdf_dict, sdf_res):
             continue
         choice = np.random.randint(len(ind), size=percentages[i][2])
         choosen_ind = ind[choice]
-        x_ind = choosen_ind % (sdf_res + 1)
-        y_ind = (choosen_ind // (sdf_res + 1)) % (sdf_res + 1)
-        z_ind = choosen_ind // (sdf_res + 1) ** 2
+        x_ind = choosen_ind % (ivt_res + 1)
+        y_ind = (choosen_ind // (ivt_res + 1)) % (ivt_res + 1)
+        z_ind = choosen_ind // (ivt_res + 1) ** 2
         x_vals = x[x_ind]
         y_vals = y[y_ind]
         z_vals = z[z_ind]
@@ -139,34 +95,22 @@ def check_insideout(cat_id, sdf_val, sdf_res, x, y, z):
     else:
         return False
 
-def create_h5_sdf_pt(cat_id, h5_file, sdf_file, flag_file, cube_obj_file, norm_obj_file,
-         centroid, m, sdf_res, num_sample, bandwidth, iso_val, max_verts, normalize):
-    sdf_dict = get_sdf(sdf_file, sdf_res)
-    ori_verts = np.asarray([0.0,0.0,0.0], dtype=np.float32).reshape((1,3))
-    print("ori_verts", ori_verts.shape)
-    samplesdf, is_insideout = sample_sdf(cat_id, num_sample, bandwidth, iso_val, sdf_dict, sdf_res)  # (N*8)x4 (x,y,z)
-    if is_insideout:
-        with open(flag_file, "w") as f:
-            f.write("mid point sdf val > 0")
-        print("insideout !!:", sdf_file)
-    else:
-        os.remove(flag_file) if os.path.exists(flag_file) else None
-    print("samplesdf", samplesdf.shape)
-    print("start to write",h5_file)
+def create_h5_ivt_pt(cat_id, h5_file, verts, faces, points_uni
+         centroid, m, ivt_res, num_sample, normalize):
+    print(verts.shape,faces.shape)
+    index = faces.reshape(-1)
+    tries = verts[index].reshape([-1,3,3])
+    # print(tries[0], faces[0], verts[:3,:])
+    sampleivt = sample_ivt_uni(cat_id, num_sample, ivt_res)  # (N*8)x4 (x,y,z)
+    print("sampleivt", sampleivt.shape)
+    print("start to write", h5_file)
     norm_params = np.concatenate((centroid, np.asarray([m]).astype(np.float32)))
     f1 = h5py.File(h5_file, 'w')
-    f1.create_dataset('pc_sdf_original', data=ori_verts.astype(np.float32), compression='gzip', compression_opts=4)
-    f1.create_dataset('pc_sdf_sample', data=samplesdf.astype(np.float32), compression='gzip', compression_opts=4)
+    f1.create_dataset('pc_ivt_sample', data=sampleivt.astype(np.float32), compression='gzip', compression_opts=4)
     f1.create_dataset('norm_params', data=norm_params, compression='gzip', compression_opts=4)
     f1.create_dataset('sdf_params', data=sdf_dict["param"], compression='gzip', compression_opts=4)
     f1.close()
-    print("end writing",h5_file)
-    command_str = "rm -rf " + norm_obj_file
-    print("command:", command_str)
-    os.system(command_str)
-    command_str = "rm -rf " + sdf_file
-    print("command:", command_str)
-    os.system(command_str)
+
 
 
 def get_normalize_mesh(model_file, norm_mesh_sub_dir):
@@ -196,35 +140,24 @@ def get_normalize_mesh(model_file, norm_mesh_sub_dir):
     obj_file = os.path.join(norm_mesh_sub_dir, "pc_norm.obj")
     ori_mesh = pymesh.load_mesh(model_file)
     print("centroid, m", centroid, m)
-    pymesh.save_mesh_raw(obj_file, (ori_mesh.vertices - centroid) / float(m), ori_mesh.faces);
+    verts = (ori_mesh.vertices - centroid) / float(m)
+    points_uni = (points_all - centroid) / float(m)
+    pymesh.save_mesh_raw(obj_file, verts, ori_mesh.faces);
     print("export_mesh", obj_file)
-    return obj_file, centroid, m
-
-def create_one_sdf(sdfcommand, res, expand_rate, sdf_file, obj_file, indx, g=0.0):
-
-    command_str = sdfcommand + " " + obj_file + " " + str(res) + " " + str(res) + \
-       " " + str(res) + " -s " + " -e " + str(expand_rate) + " -o " + str(indx) + ".dist -m 1"
-    if g > 0.0:
-        command_str += " -g " + str(g)
-    print("command:", command_str)
-    os.system(command_str)
-    command_str2 = "mv " + str(indx)+".dist " + sdf_file
-    print("command:", command_str2)
-    os.system(command_str2)
+    return verts, ori_mesh.faces, centroid, m, points_uni
 
 
-def create_sdf_obj(sdfcommand, marching_cube_command, cat_mesh_dir, cat_norm_mesh_dir, cat_sdf_dir, obj,
-                   res, iso_val, expand_rate, indx, normalize, num_sample, bandwidth,
-                   max_verts, cat_id, g, version, skip_all_exist):
+def create_ivt_obj(cat_mesh_dir, cat_norm_mesh_dir, cat_sdf_dir, obj,
+                   res, indx, normalize, num_sample, cat_id, version, skip_all_exist):
     obj=obj.rstrip('\r\n')
-    sdf_sub_dir = os.path.join(cat_sdf_dir, obj)
+    ivt_sub_dir = os.path.join(cat_sdf_dir, obj)
     norm_mesh_sub_dir = os.path.join(cat_norm_mesh_dir, obj)
-    if not os.path.exists(sdf_sub_dir): os.makedirs(sdf_sub_dir)
+    if not os.path.exists(ivt_sub_dir): os.makedirs(ivt_sub_dir)
     if not os.path.exists(norm_mesh_sub_dir): os.makedirs(norm_mesh_sub_dir)
     # sdf_file = os.path.join(sdf_sub_dir, "isosurf.sdf")
     # flag_file = os.path.join(sdf_sub_dir, "isinsideout.txt")
     # cube_obj_file = os.path.join(norm_mesh_sub_dir, "isosurf.obj")
-    h5_file = os.path.join(sdf_sub_dir, "ori_sample.h5")
+    h5_file = os.path.join(ivt_sub_dir, "ivt_sample.h5")
     if  os.path.exists(h5_file) and (skip_all_exist or not os.path.exists(flag_file)):
         print("skip existed: ", h5_file)
     else:
@@ -233,30 +166,19 @@ def create_sdf_obj(sdfcommand, marching_cube_command, cat_mesh_dir, cat_norm_mes
         else:
             model_file = os.path.join(cat_mesh_dir, obj, "models", "model_normalized.obj")
         # try:
-        print("creating", sdf_file)
         if normalize:
-            norm_obj_file, centroid, m = get_normalize_mesh(model_file, norm_mesh_sub_dir)
-
-        create_one_sdf(sdfcommand, res, expand_rate, sdf_file, norm_obj_file, indx, g=g)
-        create_one_cube_obj(marching_cube_command, iso_val, sdf_file, cube_obj_file)
-        # change to h5
-        if ish5:
-            create_h5_sdf_pt(cat_id,h5_file, sdf_file, flag_file, cube_obj_file, norm_obj_file,
-                 centroid, m, res, num_sample, bandwidth, iso_val, max_verts, normalize)
+            verts, faces, centroid, m, points_uni = get_normalize_mesh(model_file, norm_mesh_sub_dir)
+        create_h5_ivt_pt(cat_id, h5_file, verts, faces, points_uni
+            centroid, m, res, num_sample, normalize)
         # except:
         #     print("%%%%%%%%%%%%%%%%%%%%%%%% fail to process ", model_file)
 
-def create_one_cube_obj(marching_cube_command, i, sdf_file, cube_obj_file):
-    command_str = marching_cube_command + " " + sdf_file + " " + cube_obj_file + " -i " + str(i)
-    print("command:", command_str)
-    os.system(command_str)
-    return cube_obj_file
-
-def create_ivt(num_sample, res, expand_rate, cats, raw_dirs, lst_dir, ish5= True, normalize=True, version=1, skip_all_exist=False):
+def create_ivt(num_sample, res, cats, raw_dirs, lst_dir, normalize=True, version=1, skip_all_exist=False):
 
     sdf_dir=raw_dirs["ivt_dir"]
     if not os.path.exists(sdf_dir): os.makedirs(sdf_dir)
     start=0
+    unigrid = get_unigrid(res)
     for catnm in cats.keys():
         cat_id = cats[catnm]
         cat_sdf_dir = os.path.join(sdf_dir, cat_id)
@@ -270,39 +192,29 @@ def create_ivt(num_sample, res, expand_rate, cats, raw_dirs, lst_dir, ish5= True
         # print(list_obj)
         repeat = len(list_obj)
         indx_lst = [i for i in range(start, start+repeat)]
-        sdfcommand_lst=[sdfcommand for i in range(repeat)]
-        marching_cube_command_lst=[marching_cube_command for i in range(repeat)]
         cat_mesh_dir_lst=[cat_mesh_dir for i in range(repeat)]
         cat_norm_mesh_dir_lst=[cat_norm_mesh_dir for i in range(repeat)]
         cat_sdf_dir_lst=[cat_sdf_dir for i in range(repeat)]
         res_lst=[res for i in range(repeat)]
-        expand_rate_lst=[expand_rate for i in range(repeat)]
         normalize_lst=[normalize for i in range(repeat)]
-        iso_val_lst=[iso_val for i in range(repeat)]
         num_sample_lst=[num_sample for i in range(repeat)]
-        bandwidth_lst=[bandwidth for i in range(repeat)]
-        max_verts_lst=[max_verts for i in range(repeat)]
         cat_id_lst=[cat_id for i in range(repeat)]
-        g_lst=[g for i in range(repeat)]
         version_lst=[version for i in range(repeat)]
         skip_all_exist_lst=[skip_all_exist for i in range(repeat)]
-        with Parallel(n_jobs=FLAGS.thread_num) as parallel:
-            parallel(delayed(create_sdf_obj)
-            (sdfcommand, marching_cube_command, cat_mesh_dir, cat_norm_mesh_dir, cat_sdf_dir, obj, res,
-             iso_val, expand_rate, indx, norm, num_sample, bandwidth, max_verts,cat_id,g,version, skip_all_exist)
-            for sdfcommand, marching_cube_command, cat_mesh_dir, cat_norm_mesh_dir, cat_sdf_dir, obj,
-                res, iso_val, expand_rate, indx, norm, num_sample, bandwidth, max_verts,cat_id,g
-                     ,version, skip_all_exist in
-                zip(sdfcommand_lst,
-                marching_cube_command_lst,
-                cat_mesh_dir_lst,
-                cat_norm_mesh_dir_lst,
-                cat_sdf_dir_lst,
-                list_obj,
-                res_lst, iso_val_lst,
-                expand_rate_lst,
-                indx_lst, normalize_lst,num_sample_lst,
-                bandwidth_lst, max_verts_lst,cat_id_lst, g_lst, version_lst,skip_all_exist_lst))
+        # with Parallel(n_jobs=FLAGS.thread_num) as parallel:
+        #     parallel(delayed(create_ivt_obj)
+        #     (cat_mesh_dir, cat_norm_mesh_dir, cat_sdf_dir, obj, res,
+        #      indx, norm, num_sample, cat_id,version, skip_all_exist)
+        #     for cat_mesh_dir, cat_norm_mesh_dir, cat_sdf_dir, obj,
+        #         res, indx, norm, num_sample, cat_id,version, skip_all_exist in
+        #         zip(cat_mesh_dir_lst,
+        #         cat_norm_mesh_dir_lst,
+        #         cat_sdf_dir_lst,
+        #         list_obj,
+        #         res_lst, indx_lst, normalize_lst, num_sample_lst,
+        #         cat_id_lst, version_lst,skip_all_exist_lst))
+        create_ivt_obj(cat_mesh_dir_lst[0], cat_norm_mesh_dir_lst[0], cat_sdf_dir_lst[0], list_obj[0],
+            res_lst[0], indx_lst[0], normalize_lst[0], num_sample_lst[0], cat_id_lst[0], version_lst[0], skip_all_exist_lst[0])
         start+=repeat
     print("finish all")
 
@@ -319,4 +231,4 @@ if __name__ == "__main__":
         }
 
     create_ivt(32768, 256, cats, raw_dirs,
-               lst_dir, 0.003, 16384, ish5=True, normalize=True, version=1, skip_all_exist=True)
+               lst_dir, normalize=True, version=1, skip_all_exist=True)
